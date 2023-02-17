35642c71e235d51c4546227f3ab476aea18e8551
mohanadkandil <mohanadmkandil@gmail.com>
2023-02-17 13:24:13 -0800

    feat: add arrow, email, github, and linkedin svg icons

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export * from "./arrow";
+export * from "./github";
+export * from "./linkedin";
+export * from "./email";
---
+++ src/icons/linkedin.tsx
@@ -0,0 +1,24 @@
+export const LinkedIn = ({ ...props }) => {
+  return (
+    <svg
+      width={100}
+      height={100}
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_1_69)">
+        <path
+          d="M20.75 14.583C20.75 20.337 16.125 25 10.417 25 4.708 25 .083 20.337.083 14.583c0-5.75 4.625-10.416 10.334-10.416 5.708 0 10.333 4.666 10.333 10.416zm.083 18.75H0V100h20.833V33.333zm33.259 0h-20.7V100h20.704V65.004c0-19.458 25.12-21.05 25.12 0V100H100V57.788c0-32.834-37.175-31.638-45.908-15.475v-8.98z"
+          fill="#fff"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_69">
+          <path fill="#fff" d="M0 0H100V100H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
